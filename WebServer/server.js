b79c42f8be614e8f8c96716fe4c0a996d189fc3c
Minoru Horita <horita@intersystems.com>
2019-08-14 15:15:09 +0900

    some minor changes

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const express = require('express');
-
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/main-generated.html');
