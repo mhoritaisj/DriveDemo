db1ba931100fd1d6d6f8cc4700c9d712ed8c5570
Minoru Horita <horita@intersystems.com>
2019-09-02 17:59:32 +0900

    Delete run.sh

--- IRIS-MQTT/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#docker run --name iris-drive -t -p 51003:51773 -p 52003:52773 -v /Users/horita/Work/DriveData/IRIS-MQTT/projects:/projects -v /Users/horita/Work/DriveData/IRIS-MQTT/IRIS-MQTT-IoT-Adapter:/data intersystems/iris:2019.2.0.107.0
-
-docker run --name iris-drive-com -t -p 51003:51773 -p 52003:52773 -v /Users/horita/Work/DriveData/IRIS-MQTT/projects:/projects -v /Users/horita/Work/DriveData/IRIS-MQTT/IRIS-MQTT-IoT-Adapter:/data store/intersystems/iris:2019.1.0.511.0-community
-
